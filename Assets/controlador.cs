ab846515765c8641fc9ef7edce5c2503f24a397e
nichels02 <130480483+nichels02@users.noreply.github.com>
2023-11-19 22:22:45 -0500

    final final

--- Assets/controlador.cs
+++ Assets/controlador.cs
@@ -5,20 +5,35 @@ using TMPro;
 
 public class controlador : MonoBehaviour
 {
-    [SerializeField] GameObject[] paneles = new GameObject[3];
+    [SerializeField] GameObject[] paneles = new GameObject[5];
     [SerializeField] TMP_InputField valorQuerido;
     [SerializeField] TMP_Text textoFinal;
     [SerializeField] GameObject textoDeError;
     [SerializeField] calculo a;
     int valorDeseado;
-
     int[] Billetes = new int[3];
+
+    [Header("depositar")]
+    [SerializeField] TMP_InputField BilletesDe20;
+    [SerializeField] TMP_InputField BilletesDe50;
+    [SerializeField] TMP_InputField BilletesDe100;
+
+    [SerializeField] int billetesDe20;
+    [SerializeField] int billetesDe50;
+    [SerializeField] int billetesDe100;
+    [SerializeField] TMP_Text TextoDeposito;
+    int dineroIngresado=0;
+
+
     // Start is called before the first frame update
     void Start()
     {
         paneles[0].SetActive(true);
         paneles[1].SetActive(false);
         paneles[2].SetActive(false);
+        paneles[3].SetActive(false);
+        paneles[4].SetActive(false);
+        paneles[5].SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,8 +42,35 @@ public class controlador : MonoBehaviour
         if (paneles[0].active == true && Input.GetKeyDown(KeyCode.M))
         {
             paneles[0].SetActive(false);
-            paneles[1].SetActive(true);
+            paneles[3].SetActive(true);
         }
+    }
+
+    public void pedir()
+    {
+        paneles[3].SetActive(false);
+        paneles[1].SetActive(true);
+
+    }
+
+    public void entregar()
+    {
+        paneles[3].SetActive(false);
+        paneles[4].SetActive(true);
+    }
+
+    public void cuantoIngresas()
+    {
+        paneles[4].SetActive(false);
+        paneles[5].SetActive(true);
+        billetesDe20 =int.Parse(BilletesDe20.text);
+        billetesDe50 = int.Parse(BilletesDe50.text);
+        billetesDe100 = int.Parse(BilletesDe100.text);
+        dineroIngresado += billetesDe20 * 20;
+        dineroIngresado += billetesDe50 * 50;
+        dineroIngresado += billetesDe100 * 100;
+
+        TextoDeposito.text = "Gracias por elegirnos \n\nEl dinero que depositaste fue de " + dineroIngresado+" soles";
     }
 
 
